c6fb137e9cd466edb1f2a8a09bd2df0d762f2f28
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-10-31 22:27:29 +0200

    reading from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+  println!("Would you like to: ");
+  println!("\t1) View all notes");
+  println!("\t2) Add a new note");
+
+  let f = File::open("./notes.txt").unwrap();
+
+  let stdin = io::stdin();
+  let word = stdin.lock();
+  let worden = word.lines();
+  for line_ in worden {
+    let line = line_.unwrap();
+    println!("{}", line)
+  }
 }

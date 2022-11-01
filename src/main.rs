9477c5e9e03e2ad35e6ffd1b72c599e661718b93
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-11-01 18:24:38 +0200

    basic command loop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::fs::File;
-use std::io::{self, BufRead};
+// use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
-fn main() {
+fn print_commands() {
   println!("Would you like to: ");
   println!("\t1) View all notes");
   println!("\t2) Add a new note");
+  println!("\tq) To close application");
 
-  let f = File::open("./notes.txt").unwrap();
+}
+
+fn main() {
+  // let f = File::open("./notes.txt").unwrap();
+  // let reader = BufReader::new(f);
 
   let stdin = io::stdin();
-  let word = stdin.lock();
-  let worden = word.lines();
-  for line_ in worden {
-    let line = line_.unwrap();
-    println!("{}", line)
+  loop {
+    print_commands();
+    let mut buffer = String::new();
+    stdin.lock().read_line(&mut buffer);
+    match buffer.trim_end() {
+      "1" => println!("View all notes!"),
+      "2" => println!("Add new note!"),
+      "q" => break,
+      _ => println!("Unrecognized command, please try again.")
+    }
   }
 }

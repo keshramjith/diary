f652945c1b480aaf6ddd8e4796dd050be92b653e
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-11-01 22:13:25 +0200

    write/read to file in progress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use std::fs::File;
+use std::fs::{File};
 use std::io::{self, BufRead, BufReader};
 
 fn print_commands() {
@@ -9,24 +9,33 @@ fn print_commands() {
 
 }
 
+fn view_notes<T: BufRead>(reader: T) {
+  for line_ in reader.lines() {
+    let line = line_.unwrap();
+    println!("{}", line);
+  }
+}
+
 fn main() {
-  // let f = File::open("./notes.txt").unwrap();
-  // let reader = BufReader::new(f);
+  let f = File::create("notes.txt").unwrap(); // write-only, need to think around how to do reading or writing
+  let mut reader;
 
   let stdin = io::stdin();
   let mut buffer;
   loop {
     buffer = String::new();
+    reader = BufReader::new(&f);
     print_commands();
-    stdin.lock().read_line(&mut buffer);
+    stdin.lock().read_line(&mut buffer).unwrap();
     match buffer.trim_end() {
-      "1" => println!("View all notes!"),
+      "1" => view_notes(reader),
       "2" => println!("Add new note!"),
       "q" => {
-        println!("Quitting application!");
+        println!("Closing diary.");
         break;
       },
       _ => println!("Unrecognized command, please try again.")
     }
+    println!("\n");
   }
 }

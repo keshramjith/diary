d3c5e878a466bc5779b2963e0fa8e83bcd33454e
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-11-02 21:16:07 +0200

    creating a note will create notes.txt if it doesn't exist. Appends input from stdin to file. Can view these notes with option 1. Changed file io to use OpenOptions instead of File.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::{File};
-use std::io::{self, BufRead, BufReader};
+use std::fs::{OpenOptions};
+use std::io::{self, BufRead, BufReader, Stdin, Write};
 
 fn print_commands() {
   println!("Would you like to: ");
@@ -9,27 +9,40 @@ fn print_commands() {
 
 }
 
-fn view_notes<T: BufRead>(reader: T) {
-  for line_ in reader.lines() {
-    let line = line_.unwrap();
-    println!("{}", line);
+fn view_notes() {
+  let f = OpenOptions::new().read(true).open("notes.txt");
+  match f {
+    Ok(file) => {
+      let reader = BufReader::new(file);
+      for line_ in reader.lines() {
+        let line = line_.unwrap();
+        println!("{}", line);
+      }
+    },
+    Err(_) => println!("notes.txt does not exist! Please add a note!")
   }
 }
 
-fn main() {
-  let f = File::create("notes.txt").unwrap(); // write-only, need to think around how to do reading or writing
-  let mut reader;
+fn create_note(stdin: &Stdin) {
+  let mut note_buffer = String::new();
+  stdin.lock().read_line(&mut note_buffer).unwrap();
+  let mut f = OpenOptions::new().append(true).write(true).create(true).open("notes.txt").unwrap();
+  writeln!(&mut f, "{}", note_buffer).unwrap();
+}
 
+fn main() {
   let stdin = io::stdin();
-  let mut buffer;
+  let mut option_buffer;
   loop {
-    buffer = String::new();
-    reader = BufReader::new(&f);
+    option_buffer = String::new();
     print_commands();
-    stdin.lock().read_line(&mut buffer).unwrap();
-    match buffer.trim_end() {
-      "1" => view_notes(reader),
-      "2" => println!("Add new note!"),
+    stdin.lock().read_line(&mut option_buffer).unwrap();
+    match option_buffer.trim_end() {
+      "1" => view_notes(),
+      "2" => {
+        println!("Enter text to add to the note:");
+        create_note(&stdin);
+    },
       "q" => {
         println!("Closing diary.");
         break;
